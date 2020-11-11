3dd1bb3d87a965d99e38c0e669ab03b4870de4ed
xg <490599161@qq.com>
2020-11-11 18:05:58 +0800

    周刊内页的外链地址增加预览(测试)

---
+++ mp/src/pages/preview/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="main">
+    <web-view :src="url"></web-view>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      url: '',
+    }
+  },
+  onLoad(options) {
+    if (options.scene) {
+      this.url = decodeURIComponent(options.scene)
+    } else {
+      this.url = 'https://tiaocaoer.com/preview?url=' + this.$root.$mp.query.url
+    }
+  },
+}
+</script>
+
+<style lang="less" scoped>
+page {
+  background: #fff;
+}
+</style>
